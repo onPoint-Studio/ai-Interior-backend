9ee582da0829e67d9804825c6efdfacd32ec74dd
pulsito <danfrych@gmail.com>
2025-05-07 19:50:25 +0300

    Update requirements.txt

--- AI_Interior/auth/serializers.py
+++ AI_Interior/auth/serializers.py
@@ -1,6 +1,7 @@
 from rest_framework import serializers
 from django.contrib.auth.models import User
 
+
 class RegisterSerializer(serializers.ModelSerializer):
     password = serializers.CharField(write_only=True, min_length=8)
 
@@ -12,6 +13,7 @@ class RegisterSerializer(serializers.ModelSerializer):
         user = User.objects.create_user(
             username=validated_data['username'],
             email=validated_data.get('email'),
-            password=validated_data['password']
         )
+        user.set_password(validated_data['password'])
+        user.save()
         return user
--- AI_Interior/auth/urls.py
+++ AI_Interior/auth/urls.py
@@ -7,7 +7,7 @@ from .views import RegisterView
 
 urlpatterns = [
     path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
-    path('/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
-    path('/register/', RegisterView.as_view(), name='register'),
+    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('register/', RegisterView.as_view(), name='register'),
 
 ]
